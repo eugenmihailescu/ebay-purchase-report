36406e9aa5ae54d787a6c97ce354d78714763ad7
eugenmihailescu <eugenmihailescux@gmail.com>
2017-04-01 09:00:34 +0200

    minor improvements

--- script.js
+++ script.js
@@ -1,31 +1,38 @@
 /**
  * Collects the eBay purchase history data from the current page and prints-out a report in the given format.
- *
- * @params {string} format - Either 'printout' or 'json'. Default or invalid value defaults to 'printout'.
+ * 
+ * @params {string} format - Either 'printout', 'json' or 'object'. Default or invalid value defaults to 'printout'.
  */
 (function($, format) {
   format = format || 'printout';
   /**
-   * Prints-out a group subtotal
-   *
-   * @param {string} label - The group's total prefix label
-   * @param {number} value - The group's total value
-   * @param {string} currency - The group's currency for the value
-   * @param {int} count - The group's total number of items
-   * @param {int} shipped - The group's total number of shipped items
-   */
+     * Prints-out a group subtotal
+     * 
+     * @param {string}
+     *            label - The group's total prefix label
+     * @param {number}
+     *            value - The group's total value
+     * @param {string}
+     *            currency - The group's currency for the value
+     * @param {int}
+     *            count - The group's total number of items
+     * @param {int}
+     *            shipped - The group's total number of shipped items
+     */
   function printGrpFooter(label, value, currency, count, shipped) {
     console.log(sep);
     console.log(' ' + label + ': ' + Math.round(value, 2) + ' ' + currency + ', ' + shipped + ' shipped, ' + (count - shipped) + ' not-shipped (' + count + ' items, ~ ' + Math.round(value / count, 2) + ' ' + currency + '/item)');
     console.log(sep);
   }
+  
   /**
-   * Prints-out a row for the given fields by padding their values with spaces if not having a given minimum length.
-   *
-   * @param {Object} fields - An object containing the field's key name and their corresponding values
-   * @param {Object} minlen - An object containing the field's key name and their corresponding minimum length
-   */
-
+     * Prints-out a row for the given fields by padding their values with spaces if not having a given minimum length.
+     * 
+     * @param {Object}
+     *            fields - An object containing the field's key name and their corresponding values
+     * @param {Object}
+     *            minlen - An object containing the field's key name and their corresponding minimum length
+     */
   function printRow(fields, minlen) {
     var f,
       a = [],
@@ -38,17 +45,20 @@
     console.log('| ' + a.join(' | '));
   }
   /**
-   * Returns a string padded with space if not reaching a given minimum length
-   *
-   * @param {string} str - The string to pad
-   * @param {int} minlen - The desired minimum length. If the string length is less than this value then the remaining length will be padded with spaced
-   * @return {string} - Returns the padded string
-   */
-
+     * Returns a string padded with space if not reaching a given minimum length
+     * 
+     * @param {string}
+     *            str - The string to pad
+     * @param {int}
+     *            minlen - The desired minimum length. If the string length is less than this value then the remaining length
+     *            will be padded with spaced
+     * @return {string} - Returns the padded string
+     */
   function strpad(str, minlen) {
     var i = minlen - str.length;
     return str + (i > 0 ? ' '.repeat(i) : '');
   }
+  
   var sep = '-'.repeat(160);
   var i,
     e,
@@ -87,12 +97,16 @@
       });
     });
   });
-  if ('json' == format) {
+  if ('json' == format || 'object' == format) {
     var json = {};
     items.forEach(function(v, k) {
       json[k] = v;
     });
-    console.log(json);
+    if ('object' == format) {
+      console.log(json);
+    } else {
+      console.log(JSON.stringify(json));
+    }
   } else {
     console.log(sep);
     printRow({
